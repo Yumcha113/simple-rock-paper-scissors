b2643820985af956cdd24332af833ee89477be09
Andy <127144837+Yumcha113@users.noreply.github.com>
2023-08-27 23:28:58 -0700

    Add files via upload

---
+++ lim_andy_Assignment7.cpp
@@ -0,0 +1,90 @@
+// Andy Lim
+// CIS-5 Hybrid
+// 10/15/2021
+// RPS game
+
+#include <iostream>
+#include <cmath>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+
+int main() 
+{
+	
+	char ch;
+
+	int win = 0;
+	int tie = 0;
+	int lose = 0;
+
+	do {
+		int choice;
+
+		cout << "Press 1 for Rock, 2 for Paper, 3 for Scissors:" << endl;
+		cin >> choice;
+
+		int ai = rand() % 3 + 1;
+		cout << "The computer chose: " << ai << endl;
+		
+		if (choice == 1 && ai == 1) 
+		{
+			cout << "Rock meets Rock its a tie!" << endl;
+			tie++;
+		}
+		else if (choice == 1 && ai == 2)
+		{
+			cout << "Rock is covered by Paper the computer wins!." << endl;
+			lose++;
+		}
+		else if (choice == 1 && ai == 3)
+		{
+			cout << "Rock crushes Scissors you win!" << endl;
+			win++;
+		}
+		else if (choice == 2 && ai == 1)
+		{
+			cout << "Paper covers Rock you win!" << endl;
+			win++;
+		}
+		else if (choice == 2 && ai == 2) 
+		{
+			cout << "Paper meets Paper its a tie!" << endl;
+			tie++;
+		}
+		else if (choice == 2 && ai == 3) 
+		{
+			cout << "Paper is cut by Scissors the computer wins!" << endl;
+			lose++;
+		}
+		else if (choice == 3 && ai == 1) 
+		{
+			cout << "Scissors are crushed by Rock computer wins!" << endl;
+			lose++;
+		}
+		else if (choice == 3 && ai == 2) 
+		{
+			cout << "Scissors cuts Paper you win!" << endl;
+			win++;
+		}
+		else if (choice == 3 && ai == 3) 
+		{
+			cout << "Scissors meet Scissors its a tie!" << endl;
+			tie++;
+		}
+		
+		else 
+		{
+			cout << "You didn't select 1, 2, or 3" << endl;
+		}
+
+		cout << "Wins: " << win << endl;
+		cout << "Ties:" << tie << endl;
+		cout << "Losses:" << lose << endl;
+		cout << "Would you like to play again? Y/N" << endl;
+		cin >> ch;
+	} while (ch == 'Y' || ch == 'y');
+	system("pause"); 
+	return 0;
+	
+}
